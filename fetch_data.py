f734ed0376d724ec10b0b5d52ce3c4b16da9274f
jewbetcha <crollins199@gmail.com>
2021-02-05 21:56:46 -0600

    [cr] adjusted +/- model added

--- fetch_data.py
+++ fetch_data.py
@@ -2,7 +2,16 @@ import statistics
 import csv
 import pprint
 import sys
+import pandas as pd
+import numpy as np
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import Ridge
 
+def clean_dataset(df):
+    assert isinstance(df, pd.DataFrame), "df needs to be a pd.DataFrame"
+    df.dropna(inplace=True)
+    indices_to_keep = ~df.isin([np.nan, np.inf, -np.inf]).any(1)
+    return df[indices_to_keep]
 
 def goals_for_against_avg():
 	reader = csv.DictReader(open('./sample-data/teams.csv'))
@@ -24,8 +33,37 @@ def goals_for_against_avg():
 	t2_gf_ga = float(out_data[team_2]['xGF/60']) + float(out_data[team_2]['xGA/60'])
 	print(team_1 + ': ' + str(t1_gf_ga) + ', ' + team_2 + ': ' + str(t2_gf_ga) + ', AVG: ' + str(statistics.mean([t1_gf_ga, t2_gf_ga])))
 
+def adjusted_plus_minus():
+	df = pd.read_csv('./sample-data/test.csv', skiprows=1, names=['date', 'visitor', 'visitor_goals', 'home', 'home_goals'])
+	df = clean_dataset(df)
 
+	df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
+	df['goal_difference'] = df['home_goals'] - df['visitor_goals']
+
+	df['home_win'] = np.where(df['goal_difference'] > 0, 1, 0)
+	df['home_loss'] = np.where(df['goal_difference'] < 0, 1, 0)
+
+
+	df_visitor = pd.get_dummies(df['visitor'], dtype=np.int64)
+	df_home = pd.get_dummies(df['home'], dtype=np.int64)
+
+	df_model = df_home.sub(df_visitor) 
+	df_model['goal_difference'] = df['goal_difference']
+
+	
+	df_train = df_model
+
+	## Not sure why but the data is kinda broken and the sharks column comes up empty and breaks everything /shrug
+	del df_train['San Jose Sharks']
+
+	lr = Ridge(alpha=0.001) 
+	X = df_train.drop(['goal_difference'], axis=1)
+	y = df_train['goal_difference']
+	lr.fit(X, y)
+
+	df_ratings = pd.DataFrame(data={'team': X.columns, 'rating': lr.coef_})
+	print(df_ratings.sort_values(by=['rating']))
 
 
 if __name__ == '__main__':
-	goals_for_against_avg()
+	adjusted_plus_minus()
--- server.py
+++ server.py
@@ -3,6 +3,10 @@ from flask import request
 from flask_cors import CORS
 import statistics
 import csv
+import pandas as pd
+import numpy as np
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import Ridge
 
 app = Flask(__name__)
 CORS(app)
@@ -13,7 +17,7 @@ def goals_for_against_avg():
 	team_1 = teams['team_1']
 	team_2 = teams['team_2']
 
-	reader = csv.DictReader(open('./sample-data/teams.csv'))
+	reader = csv.DictReader(open('./sample-data/teams1.csv'))
 	data = []
 	for row in reader:
 		data.append(row)
@@ -34,6 +38,25 @@ def goals_for_against_avg():
 		'team_1': t1_gf_ga,
 		'team_2': t2_gf_ga
 	}
+
+@app.route('/adjusted-plus-minus', methods=['POST'])
+def adjusted_plus_minus():
+	df = pd.read_csv('./sample-data/test.csv', skiprows=1, names=['date', 'visitor', 'visitor_goals', 'home', 'home_goals'])
+
+	df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
+	df['goal_difference'] = df['home_goals'] - df['visitor_goals']
+	df['home_win'] = np.where(df['goal_difference'] > 0, 1, 0)
+	df['home_loss'] = np.where(df['goal_difference'] < 0, 1, 0)
+	df_visitor = pd.get_dummies(df['visitor'], dtype=np.int64)
+	df_home = pd.get_dummies(df['home'], dtype=np.int64)
+	df_model = df_home.sub(df_visitor) 
+	df_model['goal_difference'] = df['goal_difference']
+	lr = Ridge(alpha=0.001) 
+	X = df_train.drop(['goal_difference'], axis=1)
+	y = df_train['goal_difference']
+
+	lr.fit(X, y)
+	df_ratings = pd.DataFrame(data={'team': X.columns, 'rating': lr.coef_})
 
 
 if __name__ == '__main__':
