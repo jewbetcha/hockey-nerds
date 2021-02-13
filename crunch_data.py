e575abd3e033846c4e7ac8384cc7936f46c24139
jewbetcha <crollins199@gmail.com>
2021-02-13 07:32:05 -0600

    [cr] working model for basketball with default data

--- crunch_data.py
+++ crunch_data.py
@@ -33,8 +33,8 @@ def goals_for_against_avg():
 	t2_gf_ga = float(out_data[team_2]['xGF/60']) + float(out_data[team_2]['xGA/60'])
 	print(team_1 + ': ' + str(t1_gf_ga) + ', ' + team_2 + ': ' + str(t2_gf_ga) + ', AVG: ' + str(statistics.mean([t1_gf_ga, t2_gf_ga])))
 
-def adjusted_plus_minus():
-	df = pd.read_csv('./sample-data/2021data.csv', skiprows=1, names=['date', 'visitor', 'visitor_goals', 'home', 'home_goals'])
+def adjusted_plus_minus_hockey(data_file):
+	df = pd.read_csv(data_file, skiprows=1, names=['date', 'visitor', 'visitor_goals', 'home', 'home_goals'])
 	df = clean_dataset(df)
 
 	df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
@@ -50,7 +50,6 @@ def adjusted_plus_minus():
 	df_model = df_home.sub(df_visitor) 
 	df_model['goal_difference'] = df['goal_difference']
 
-	
 	df_train = df_model
 
 	## Not sure why but the data is kinda broken and the sharks column comes up empty and breaks everything /shrug
@@ -66,5 +65,36 @@ def adjusted_plus_minus():
 
 # TODO: add stats and asjusted_plus_minus for basketball too
 
+def adjusted_plus_minus_basketball(data_file):
+	cleaned_file = pd.read_csv(data_file)
+	columns_to_keep = ['Visitor', 'visitor_pts', 'Home', 'home_pts']
+	cleaned_file = cleaned_file[columns_to_keep]
+	file = cleaned_file.to_csv('./sample-data/out.csv')
+	df = pd.read_csv('./sample-data/out.csv', skiprows=1, names=['date','visitor', 'visitor_goals', 'home', 'home_goals'])
+
+	df['goal_difference'] = df['home_goals'] - df['visitor_goals']
+
+	df['home_win'] = np.where(df['goal_difference'] > 0, 1, 0)
+	df['home_loss'] = np.where(df['goal_difference'] < 0, 1, 0)
+
+
+	df_visitor = pd.get_dummies(df['visitor'], dtype=np.int64)
+	df_home = pd.get_dummies(df['home'], dtype=np.int64)
+
+	df_model = df_home.sub(df_visitor) 
+	df_model['goal_difference'] = df['goal_difference']
+
+	
+	df_train = df_model
+
+	lr = Ridge(alpha=0.001) 
+	X = df_train.drop(['goal_difference'], axis=1)
+	y = df_train['goal_difference']
+	lr.fit(X, y)
+
+	df_ratings = pd.DataFrame(data={'team': X.columns, 'rating': lr.coef_})
+	print(df_ratings.sort_values(by=['rating']))
+
 if __name__ == '__main__':
-	adjusted_plus_minus()
+	adjusted_plus_minus_hockey('./sample-data/2021data.csv')
+	adjusted_plus_minus_basketball('./sample-data/2021_basketball_data.csv')
